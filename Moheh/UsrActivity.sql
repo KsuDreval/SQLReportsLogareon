868950affd6829fd93b60681bc4e9e3811ebe8f1
Ksenia Dreval <92667953+KsuDreval@users.noreply.github.com>
2025-06-19 10:09:47 +0300

    Update UsrActivity.sql
    обновленная версия

--- Moheh/UsrActivity.sql
+++ Moheh/UsrActivity.sql
@@ -1,22 +1,24 @@
 select
-to_char("Date", 'DD-MM-YYYY') as date1
+"Date"::date as date1
 , e."Name"
 , "Queue"
 , count(*)
 from
-	(select lm."Date", lm."AssigneeRef", lmd."Queue", lm."StateName" 
+	(select lm."Date", lm."AssigneeRef", lmd."Queue" 
 	from locationmovement lm
 	left join locationmovementdatamart lmd on lmd."ObjectId" = lm."uuid"
 	where lm."StateName" = 'Completed'
 	union all
-	select sm."Date", sm."AssigneeRef", smd."Queue", sm."StateName" 
+	select sm."Date", sm."AssigneeRef", smd."Queue" 
 	from skumovement sm
 	left join skumovementdatamart smd on smd."ObjectId" = sm."uuid"
 	where sm."StateName" = 'Completed'
 	union all
-	select ct."Date", ct."AssigneeRef", ctd."Queue", ct."StateName" 
+	select ct."Date", ct."AssigneeRef", ctd."Queue" 
 	from countingtask ct
 	left join countingdatamart ctd on ctd."ObjectId" = ct."uuid"
 	where ct."StateName" = 'Completed') as tasks
 left join employee e on e."uuid" = "AssigneeRef"
+where e."Name" != 'System'
 group by date1, e."Name", "Queue"
+order by date1 desc
